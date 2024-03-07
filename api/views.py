4709eab27643259175a1feae2940b262eb553969
saba-ab <sabaabjandadze@gmail.com>
2024-03-08 00:50:38 +0400

    feat: Implement serializers and API endpoints for models

--- api/views.py
+++ api/views.py
@@ -1,3 +1,20 @@
 from django.shortcuts import render
-
+from rest_framework import viewsets
+from .models import Exercise, WorkoutPlan, ProgressTracking
+from .serializers import ExerciseSerializer, WorkoutPlanSerializer, ProgressTrackingSerializer
 # Create your views here.
+
+
+class ExerciseViewSet(viewsets.ModelViewSet):
+    queryset = Exercise.objects.all()
+    serializer_class = ExerciseSerializer
+
+
+class WorkoutPlanViewSet(viewsets.ModelViewSet):
+    queryset = WorkoutPlan.objects.all()
+    serializer_class = WorkoutPlanSerializer
+
+
+class ProgressTrackingViewSet(viewsets.ModelViewSet):
+    queryset = ProgressTracking.objects.all()
+    serializer_class = ProgressTrackingSerializer
