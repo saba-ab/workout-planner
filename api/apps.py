9512ac80e8c7a898fa4253698e238c906301def8
saba-ab <sabaabjandadze@gmail.com>
2024-03-12 18:31:49 +0400

    Fix app initialization db access

--- api/apps.py
+++ api/apps.py
@@ -6,13 +6,4 @@ class ApiConfig(AppConfig):
     name = 'api'
 
     def ready(self):
-        from django.core.management import call_command
-        from django.db.utils import OperationalError
-        from .models import Exercise
-        try:
-            if not Exercise.objects.exists():
-                call_command('loaddata', 'exercises.json')
-        except OperationalError:
-            # raise OperationalError(
-            #     "Database table for Exercise model doesn't exist. Did you forget to migrate? Original error: {}".format(e))
-            pass
+        from . import signals
---
+++ api/signals.py
@@ -0,0 +1,16 @@
+# api/signals.py
+
+from django.core.management import call_command
+from django.db.models.signals import post_migrate
+from django.dispatch import receiver
+from django.db.utils import OperationalError
+
+
+@receiver(post_migrate)
+def load_initial_exercise_data(sender, **kwargs):
+    from .models import Exercise
+    try:
+        if not Exercise.objects.exists():
+            call_command('loaddata', 'exercises.json')
+    except OperationalError:
+        pass
